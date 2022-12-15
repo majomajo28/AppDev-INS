305b3d869afd8902fb786a6d85770d5ea1c61fa6
majomajo28 <102529236+majomajo28@users.noreply.github.com>
2022-12-15 12:02:16 +0800

    Update MyCsharp.cs

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -2,15 +2,14 @@ using System;
 					
 public class MyCsharp
 {
-	public static void Main(string[] args)
+	public static void Main(string lastName, string firstName)
 	{
-		// Insert code from line below this comment where it instantiates the MyCsharp object into variable named pin, then call the method named printName(String name);
-    // Expected Line 1, replace this comment with code on pull request
-    // Expected Line 2, replace this comment with code on pull request
+		lastName = SUMAGUE;
+		firstName = MARJORIEANN;
 	}
 	
-	public void printName(string name)
+	public void printName(string lastName, firstName)
 	{
-		Console.WriteLine("Hello! My name is " + name);
+		Console.WriteLine("Hello! My name is " + LastName+"_"+firstName);
 	}
 }
